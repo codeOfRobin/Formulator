6e4edb7052dd9cf9b7c525e88c3134d4a4b263eb
codeOfRobin <me@rmalhotra.com>
2016-08-12 16:39:11 +0530

    before I add promisekit

--- Formulator/ViewController.swift
+++ Formulator/ViewController.swift
@@ -7,24 +7,51 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
 	var prop1 = 0
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		let button = UIButton()
-		button.actionHandle(controlEvents: UIControlEvents.TouchUpInside,
-		                    ForAction:{() -> Void in
-								self.prop1 +=1
-								print(self.prop1)
-								
-		})
+		let button = FormButton()
+		button.primaryClosure = {
+			self.prop1 += 1
+			print(self.prop1)
+		}
 		button.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
 		view.addSubview(button)
 		button.setTitle("ASDfasd", forState: .Normal)
 		button.setTitleColor(UIColor.blueColor(), forState: .Normal)
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		let textField = FormTextField(frame: CGRect(x: 0, y: 300, width: 300, height: 50))
+		textField.validateText = {
+			print((textField.text?.containsString("asd"))!)
+			return (textField.text?.containsString("asd"))!
+		}
+		
+		textField.gatherSuggestions = {
+			
+			Alamofire.request(.GET, "https://a4825bf2.ngrok.io", parameters: nil, encoding: .JSON, headers: nil).validate().responseJSON(completionHandler: { (response) in
+				switch response.result {
+				case .Success:
+					if let value = response.result.value {
+						let json = JSON(value)
+						print("JSON: \(json)")
+					}
+					case .Failure(let error):
+						print(error)
+					}
+			})
+		}
+		textField.borderStyle = .Bezel
+		view.addSubview(textField)
+		let red = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+		red.backgroundColor = UIColor.redColor()
+		textField.addSubview(red)
+		textField.clipsToBounds = false
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -35,23 +62,48 @@ class ViewController: UIViewController {
 
 }
 
+typealias EmptyClosure = () -> ()
+typealias ValidationClosure = () -> Bool
+typealias SuggestionsClosure = () -> [String]
 
-
-extension UIButton {
+class FormButton: UIButton {
 	
-	
-	private func actionHandleBlock(action:(() -> Void)? = nil) {
-		if let closure = action {
-			closure()
+	var primaryClosure: EmptyClosure = {} {
+		didSet {
+			self.removeTarget(nil, action: nil, forControlEvents: .PrimaryActionTriggered)
+			self.addTarget(self, action: #selector(FormButton.buttonTapped(_:)),forControlEvents: .PrimaryActionTriggered)
 		}
 	}
 	
-	@objc private func triggerActionHandleBlock() {
-		self.actionHandleBlock()
-	}
 	
-	func actionHandle(controlEvents control :UIControlEvents, ForAction action:() -> Void) {
-		self.addTarget(self, action: #selector(UIButton.triggerActionHandleBlock), forControlEvents: control)
+	@objc private func buttonTapped(sender: UIButton) {
+		primaryClosure()
 	}
 }
 
+class FormTextField: UITextField {
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		self.initialize()
+	}
+	
+	required init(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)!
+		self.initialize()
+	}
+	
+	func initialize() {
+		self.addTarget(self, action: #selector(FormTextField.textFieldChanged(_:)),forControlEvents: .EditingChanged)
+	}
+	
+	var validateText: ValidationClosure = {return false}
+	
+	var gatherSuggestions: EmptyClosure = {}
+	
+	@objc private func textFieldChanged(sender: UIButton) {
+		validateText()
+		gatherSuggestions()
+	}
+}
